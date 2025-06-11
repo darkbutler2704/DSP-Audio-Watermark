397e271c91f94a39d6a9b9a5db66c27c4f4cb137
Thristian Cabuang <119090932+darkbutler2704@users.noreply.github.com>
2025-06-11 22:19:40 +0800

    Update AudioWatermark.py

--- AudioWatermark.py
+++ AudioWatermark.py
@@ -420,11 +420,9 @@ def main():
         - Higher frequencies are less noticeable
 
         **For Embedding**
-            | Message Length      | Start Frequency | Width        | Strength |
-            | ------------------- | --------------- | ------------ | -------- |
-            | Short (4–8 chars)   | 2000 Hz         | 1000–1500 Hz | 0.3      |
-            | Medium (8–16 chars) | 3000–4000 Hz    | 1500–2000 Hz | 0.4–0.5  |
-            | Long (16–32+ chars) | 5000+ Hz        | 2000–3000 Hz | 0.5–0.6  |
+        - Short (4–8 chars) = 2000 Hz, 1000–1500 Hz, 0.3
+        - Medium (8–16 chars) = 3000–4000 Hz, 1500–2000 Hz, 0.4–0.5
+        - Long (16–32+ chars) = 5000+ Hz, 2000–3000 Hz, 0.5–0.6
                         
         **If Error:**
         - If it says "operands could not be broadcast together with shapes", convert first the audio to mono if stereo
